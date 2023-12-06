34c52ff0438b3ac14645f12923a7503cf0242c25
haomingbai <haomingbai@hotmail.com>
2023-12-06 19:46:52 +0800

    modified:   .gitignore 	new file:   report/algorithm.h 	new file:   report/file.h 	new file:   report/main.cpp 	new file:   report/mindmap1.png 	new file:   report/program-output.png 	modified:   report/report.pdf 	modified:   report/report.tex 	new file:   report/showcase.png

---
+++ report/algorithm.h
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+void data_process(int *bitmap,int *rectan){
+	bitmap+=*rectan;
+	for(int i=*rectan;i<*(rectan+2);i++){
+		if(*bitmap<*(rectan+1)) *bitmap=*(rectan+1);
+		//cout<<*bitmap<<" "<<i<<endl;//测试用代码
+		bitmap++;
+	}
+}
+
+//测试用代码
+void read_data(int *p,int n){
+	for(int i=0;i<n;i++){
+		printf("%d.%d ",i,*p);
+		p++;
+	}
+	printf("\n");//
+}
+
+void key_vector(int *p,int length){
+	int *a=p;
+	for(int i=1;i<=length;i++){
+		//cout<<a<<endl;//测试用代码
+		if(*a==*(a+1)){
+			a++;
+			continue;
+			//cout<<"*a="<<*a<<" *(a+1)="<<*(a+1)<<endl;//测试用代码
+		}else{
+			printf("%d %d ",i,*(a+1));
+		}
+		a++;
+	}
+	printf("\n");
+}
---
+++ report/file.h
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+
+
+//Count the number of lines of a file
+int countline(char *filename)
+{
+    ifstream ReadFile;
+    int n=0;
+    string tmp;
+    ReadFile.open(filename,ios::in);//ios::in 表示以只读的方式读取文件
+    if(ReadFile.fail())//文件打开失败:返回0
+    {
+        return -1;
+    }
+    else//文件存在
+    {
+        while(getline(ReadFile,tmp,'\n'))
+        {
+            n++;
+        }
+        ReadFile.close();
+        return n;
+    }
+}
+
+
+
+//This function can return the max x coordinate value
+int readfile(string filename,int d[][3],int linenum){
+	ifstream fin;
+	fin.open(filename,ios::in);
+	int **n,**p,**x;
+	n=(int **)malloc(linenum*sizeof(int *));
+	p=n;
+    x=n;
+	for(int i=0;i<linenum;i++){
+		*p=d[i];
+		p++;
+	}
+	int m=0;
+	char buff[32];
+	while(fin.getline(buff,sizeof(buff))){
+		sscanf(buff,"%d %d %d",*x,(*x+1),(*x+2));
+		//cout<<buff<<endl;
+		//printf("%d %d %d\n",**n,*(*n+1),*(*n+2));//测试用代码
+		if(**x>=m) m=**x;
+		if(*(*x+2)>=m) m=*(*x+2);
+		x++;
+	}
+    free(n);
+    fin.close();
+	return m;
+}
+
+
